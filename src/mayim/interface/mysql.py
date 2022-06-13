dcd094a3ed6732a9ae4a31b377d29367c5ecfb15
Adam Hopkins <adam@amhopkins.com>
2022-06-13 13:54:50 +0300

    Remove unnecesary import

--- src/mayim/interface/mysql.py
+++ src/mayim/interface/mysql.py
@@ -1,4 +1,4 @@
-from typing import AsyncContextManager, Optional
+from typing import Optional
 
 import asyncmy
 
--- tests/test_sql_loading.py
+++ tests/test_sql_loading.py
@@ -91,3 +91,36 @@ async def test_auto_load_positional(postgres_connection):
     postgres_connection.execute.assert_called_with(
         EXPECTED_POSITIONAL.text, [4, 0]
     )
+
+
+async def test_sql_decorator_positional(postgres_connection):
+    postgres_connection.result = {"item_id": 99, "name": "thing"}
+
+    class ItemExecutor(PostgresExecutor):
+        @sql(
+            """SELECT *
+            FROM otheritems
+            LIMIT $1 OFFSET $2;
+            """
+        )
+        async def select_items_numbered(
+            self, limit: int = 4, offset: int = 0
+        ) -> Item:
+            ...
+
+    Mayim(executors=[ItemExecutor], dsn="fake")
+    executor = Mayim.get(ItemExecutor)
+
+    assert (
+        ItemExecutor._queries["select_items_numbered"] != EXPECTED_POSITIONAL
+    )
+    assert (
+        ItemExecutor._queries["select_items_numbered"].param_type
+        is ParamType.POSITIONAL
+    )
+    await executor.select_items_numbered(limit=10, offset=40)
+
+    query_text = EXPECTED_POSITIONAL.text.replace(
+        "items", "otheritems"
+    ).strip()
+    postgres_connection.execute.assert_called_with(query_text, [10, 40])

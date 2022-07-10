72b2d38a6924a16495c8e1be5c5dfb6d8874855a
Adam Hopkins <adam@amhopkins.com>
2022-07-10 14:39:58 +0300

    Cleanup test with registry resets

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,12 @@ import pytest
 
 from mayim.executor import Executor
 from mayim.interface import postgres
-from mayim.registry import Registry
+from mayim.registry import (
+    InterfaceRegistry,
+    LazyHydratorRegistry,
+    LazySQLRegistry,
+    Registry,
+)
 
 from .app.model import Foo
 
@@ -29,6 +34,9 @@ class PostgresConnectionMock:
 @pytest.fixture(autouse=True)
 def reset_registry():
     Registry().reset()
+    InterfaceRegistry().reset()
+    LazySQLRegistry().reset()
+    LazyHydratorRegistry().reset()
 
 
 @pytest.fixture

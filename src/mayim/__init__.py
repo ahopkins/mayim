06f4fb6f8361de554c3299549811056417dfaf51
Adam Hopkins <adam@amhopkins.com>
2022-08-12 11:19:08 +0300

    Add extra install

--- src/mayim/__init__.py
+++ src/mayim/__init__.py
@@ -1,18 +1,24 @@
 from .base import Executor, Hydrator
 from .decorator import hydrator, register, sql
 from .impl.sql.mysql.executor import MysqlExecutor
+from .impl.sql.mysql.interface import MysqlPool
 from .impl.sql.postgres.executor import PostgresExecutor
+from .impl.sql.postgres.interface import PostgresPool
 from .impl.sql.sqlite.executor import SQLiteExecutor
+from .impl.sql.sqlite.interface import SQLitePool
 from .mayim import Mayim
 
 __all__ = (
+    "hydrator",
+    "register",
+    "sql",
     "Executor",
     "Hydrator",
     "Mayim",
     "MysqlExecutor",
+    "MysqlPool",
     "PostgresExecutor",
     "SQLiteExecutor",
-    "hydrator",
-    "register",
-    "sql",
+    "PostgresPool",
+    "SQLitePool",
 )

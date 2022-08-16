64ccaf425d68a568c34facdf1ba6dd673a9998da
Adam Hopkins <adam@amhopkins.com>
2022-08-16 10:05:33 +0300

    Reduce apiPages

--- docs/src/.vuepress/apiPages.js
+++ docs/src/.vuepress/apiPages.js
@@ -1,33 +1,4 @@
 
 module.exports = {
-    apiPages: [
-        '/api/',
-        '/api/mayim.base.md',
-        '/api/mayim.base.executor.md',
-        '/api/mayim.base.hydrator.md',
-        '/api/mayim.base.interface.md',
-        '/api/mayim.base.query.md',
-        '/api/mayim.convert.md',
-        '/api/mayim.decorator.md',
-        '/api/mayim.exception.md',
-        '/api/mayim.extension.md',
-        '/api/mayim.extension.quart_extension.md',
-        '/api/mayim.extension.sanic_extension.md',
-        '/api/mayim.extension.starlette_extension.md',
-        '/api/mayim.extension.statistics.md',
-        '/api/mayim.lazy.interface.md',
-        '/api/mayim.mayim.md',
-        '/api/mayim.registry.md',
-        '/api/mayim.sql.executor.md',
-        '/api/mayim.sql.mysql.executor.md',
-        '/api/mayim.sql.mysql.interface.md',
-        '/api/mayim.sql.mysql.query.md',
-        '/api/mayim.sql.postgres.executor.md',
-        '/api/mayim.sql.postgres.interface.md',
-        '/api/mayim.sql.postgres.query.md',
-        '/api/mayim.sql.query.md',
-        '/api/mayim.sql.sqlite.executor.md',
-        '/api/mayim.sql.sqlite.interface.md',
-        '/api/mayim.sql.sqlite.query.md'
-    ]
+    apiPages: []
 }
